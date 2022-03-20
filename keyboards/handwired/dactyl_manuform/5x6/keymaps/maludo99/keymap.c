80b139f0396a0743ae2659f1db326bdeef86406e
Dario Schmid <darioschmid99@outlook.com>
2022-03-20 13:44:14 +0100

    changed ordering of layers

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/maludo99/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/maludo99/keymap.c
@@ -73,17 +73,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         _______,_______,                        _______,_______,
                                         _______,_______,                        _______,_______
     ),
-	
-    [PRACT] = LAYOUT_5x6(
-        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,XXXXXXX,
-        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_B  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,XXXXXXX,
-        KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_T  ,                         KC_M  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_ENT ,
-        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_G  , KC_V  ,                         KC_N  , KC_H  ,KC_COMM,KC_DOT ,KC_SLSH,XXXXXXX,
-                        XXXXXXX,XXXXXXX,                                                        XXXXXXX,XXXXXXX,
-                                _______,_______,                                        _______,PRACTSP,
-                                        _______,_______,                        _______,_______,
-                                        _______,_______,                        _______,_______
-    ),
 
     [SPECIAL] = LAYOUT_5x6(
         DE_CIRC,DE_DEG ,DE_SUP2,DE_SUP3,DE_DLR ,DE_EURO,                        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
@@ -114,6 +103,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,XXXXXXX, PRACT ,GAMING ,QWERTZ ,XXXXXXX,                        XXXXXXX,KC_VOLD,KC_MUTE,KC_VOLU,XXXXXXX,KC_F15 ,
                         XXXXXXX,XXXXXXX,                                                        XXXXXXX,XXXXXXX,
                                 _______,_______,                                        _______,_______,
+                                        _______,_______,                        _______,_______,
+                                        _______,_______,                        _______,_______
+    ),
+	
+    [PRACT] = LAYOUT_5x6(
+        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,XXXXXXX,
+        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_B  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,XXXXXXX,
+        KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_T  ,                         KC_M  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_ENT ,
+        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_G  , KC_V  ,                         KC_N  , KC_H  ,KC_COMM,KC_DOT ,KC_SLSH,XXXXXXX,
+                        XXXXXXX,XXXXXXX,                                                        XXXXXXX,XXXXXXX,
+                                _______,_______,                                        _______,PRACTSP,
                                         _______,_______,                        _______,_______,
                                         _______,_______,                        _______,_______
     ),
